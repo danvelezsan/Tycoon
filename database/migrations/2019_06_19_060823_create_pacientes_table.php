63dc345f264ce136ee68a561b2629d55a4c205e2
Daniel <danvelezsan@unal.edu.co>
2019-06-29 15:54:03 -0500

    2343

---
+++ database/migrations/2019_06_19_060823_create_pacientes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePacientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->date('fecha_nacimiento');
+            $table->string('genero');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pacientes');
+    }
+}
