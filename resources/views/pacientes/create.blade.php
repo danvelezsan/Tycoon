097c02c85ee08478f2c57937b774785b86ed77ca
Juan Misas <juanfer.misas@outlook.com>
2019-06-29 22:32:41 -0500

    commit prron

--- resources/views/pacientes/create.blade.php
+++ resources/views/pacientes/create.blade.php
@@ -5,41 +5,83 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Registrar Paciente') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('pacientes.store') }}">
                         @csrf
 
-                        <div class="form-group">    
-                            <label for="id">Cedula:</label>
-                            <input type="number" class="form-control" name="id"/>
+                        <div class="form-group row">
+                            <label for="id" class="col-md-4 col-form-label text-md-right">Cédula</label>
+
+                            <div class="col-md-6">
+                                <input id="id" type="number" class="form-control @error('id') is-invalid @enderror" name="id" value="{{ old('id') }}" required autocomplete="id" autofocus placeholder="Ingrese su cédula" min="0">
+
+                                @error('id')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
 
-                        <div class="form-group">    
-                            <label for="nombre">Nombre:</label>
-                            <input type="text" class="form-control" name="nombre"/>
+                        <div class="form-group row">
+                            <label for="nombre" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus placeholder="Ingrese su nombre">
+
+                                @error('nombre')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
 
-                        <div class="form-group">    
-                            <label for="apellidos">Apellidos:</label>
-                            <input type="text" class="form-control" name="apellidos"/>
+                        <div class="form-group row">
+                            <label for="apellidos" class="col-md-4 col-form-label text-md-right">{{ __('Apellidos') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="apellidos" type="text" class="form-control @error('apellidos') is-invalid @enderror" name="apellidos" value="{{ old('apellidos') }}" required autocomplete="apellidos" autofocus placeholder="Ingrese sus apellidos">
+
+                                @error('apellidos')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
 
-                        <div class="form-group">    
-                            <label for="fecha_nacimiento">Fecha de Nacimiento:</label>
-                            <input type="date" class="form-control" name="fecha_nacimiento"/>
+                        <div class="form-group row">
+                            <label for="fecha_nacimiento" class="col-md-4 col-form-label text-md-right">{{ __('Fecha de nacimiento') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="fecha_nacimiento" type="date" class="form-control @error('fecha_nacimiento') is-invalid @enderror" name="fecha_nacimiento" value="{{ old('fecha_nacimiento') }}" required autocomplete="fecha_nacimiento" autofocus placeholder="Ingrese su fecha de nacimiento">
+
+                                @error('fecha_nacimiento')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
 
-                        <div class="form-group">    
-                            <label for="genero">Genero:</label>
-                            <input type="text" class="form-control" name="genero"/>
+                        <div class="form-group row">
+                            <label for="genero" class="col-md-4 col-form-label text-md-right">{{ __('Género') }}</label>
+
+                            <div class=" col-md-6">
+                                <select name="genero" required id="genero" class="form-control">
+                                    <option value="masculino">Masculino</option>
+                                    <option value="femenino">Femenino</option>
+                                </select>
+                            </div>
                         </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('Registrar') }}
                                 </button>
                             </div>
                         </div>
