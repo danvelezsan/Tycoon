bbf54b4b418b3865bca6a46ff329bd8aca755ade
Daniel <danvelezsan@unal.edu.co>
2019-06-30 18:50:33 -0500

    Medicos Provisionales

---
+++ app/MedicoGeneral.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MedicoGeneral extends Model
+{
+    public $timestamp = false;
+
+    protected $fillable = [
+        'cedula', 'nombre', 'apellidos','fecha_nacimiento','genero', 'tarjeta_profesional'
+    ];
+
+    protected $casts = [
+        'fecha_nacimiento' => 'date',
+    ];
+
+}
---
+++ database/migrations/2019_06_30_213209_create_medico_especialistas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicoEspecialistasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medico_especialistas', function (Blueprint $table) {
+            $table->integer('cedula')->unique();
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->date('fecha_nacimiento');
+            $table->string('genero');
+            $table->integer('tarjeta_profesional');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medico_especialistas');
+    }
+}
