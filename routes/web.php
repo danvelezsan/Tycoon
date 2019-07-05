07dd8036078849611ca90e8dff655f230602783a
Juan Misas <juanfer.misas@outlook.com>
2019-07-05 01:10:23 -0500

    lelo

--- routes/web.php
+++ routes/web.php
@@ -20,22 +20,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/administrador/inicio', 'AdministradorController@index');
 Route::resource('administrador', 'AdministradorController');
 
-Route::get('/pacientes/registrarPaciente', 'PacienteController@create');
-Route::get('/pacientes/inicio', 'PacienteController@index');
-Route::get('/pacientes/listarPacientes', 'PacienteController@show');
-Route::delete('/pacientes/eliminarPaciente/{id}', 'PacienteController@destroy');
-Route::resource('pacientes', 'PacienteController');
-
-Route::get('/medicosGenerales/registrarMedicoGeneral', 'MedicoGeneralController@create');
-Route::get('/medicosGenerales/listarMedicosGenerales', 'MedicoGeneralController@show');
-Route::delete('/medicosGenerales/eliminarMedicoGeneral/{id}', 'MedicoGeneralController@destroy');
-Route::resource('medicosGenerales', 'MedicoGeneralController');
-
-Route::get('/medicosEspecialistas/registrarMedicoEspecialista', 'MedicoEspecialistaController@create');
-Route::get('/medicosEspecialistas/listarMedicosEspecialistas', 'MedicoEspecialistaController@show');
-Route::delete('/medicosEspecialistas/eliminarMedicoEspecialista/{id}', 'MedicoEspecialistaController@destroy');
-Route::resource('medicosEspecialistas', 'MedicoEspecialistaController');
-
 Route::get('/pacientes/registrarPaciente', 'PacienteController@create')->middleware('auth', 'role:Administrador');
 Route::get('/pacientes', 'PacienteController@index');
 Route::get('/pacientes/listarPacientes', 'PacienteController@show')->middleware('auth', 'role:Administrador');
